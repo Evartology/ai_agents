f2baeaf9ddcaafd7d3f10ea30c69fba463fb9f3a
Evartology <evartology@gmail.com>
2025-06-17 13:25:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/ai_agents/',
   plugins: [react()],
   resolve: {
     alias: {
